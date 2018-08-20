ec7485c5bb4b27e6ccbe2d75fbdb869fe05a2fcf
rikhsan <rakhmad.ikhsanudin@gmail.com>
2018-08-21 04:03:06 +0700

    asd

--- jurica/views.py
+++ jurica/views.py
@@ -392,38 +392,6 @@ def table_responden(request):
 
 import xlwt
 @decoadmin
-def export_xls(request):
-    response = HttpResponse(content_type='application/ms-excel')
-    response['Content-Disposition'] = 'attachment; filename="Report Studi Akuntansi.xls"'
-
-    wb = xlwt.Workbook(encoding='utf-8')
-    ws = wb.add_sheet('Responden')
-
-    # Sheet header, first row
-    row_num = 0
-
-    font_style = xlwt.XFStyle()
-    font_style.font.bold = True
-
-    columns = ['id', 'nama', 'email', 'nomor_hp', 'jenis_kelamin', 'usia', 'pendidikan', 'program', 'semester', 'masa_kerja', 'status', 'score']
-
-    for col_num in range(len(columns)):
-        ws.write(row_num, col_num, columns[col_num], font_style)
-
-    # Sheet body, remaining rows
-    font_style = xlwt.XFStyle()
-
-    rows = Responden.objects.all().values_list('id_responden', 'nama', 'email', 'nomor_hp', 'jenis_kelamin', 'usia', 'pendidikan', 'program', 'semester', 'masa_kerja', 'status', 'score')
-    for row in rows:
-        row_num += 1
-        for col_num in range(len(row)):
-            ws.write(row_num, col_num, row[col_num], font_style)
-
-    wb.save(response)
-    return response
-
-import xlwt
-@decoadmin
 def export_xls2(request):
 	response = HttpResponse(content_type='application/ms-excel')
 	response['Content-Disposition'] = 'attachment; filename="Report Studi Akuntansi.xls"'
