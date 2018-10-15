9931e88a88d4e86e5c03cf4b54d39cdb43047898
Your Name <rakhmad.ikhsanudin@gmail.com>
2018-10-15 07:33:32 +0000

    sd

--- quesioner/settings.py
+++ quesioner/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y+l!#8opt0x*99rconq#yqv^-p#tfhb@s8!9pcjeyn$-2aek2q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['178.128.110.195','localhost','jurica.rikhsan.com']
 
